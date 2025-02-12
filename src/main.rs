47aa6f71abfaebe6846ad4d4d73c1a8493d3d71e
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2025-02-12 22:12:59 +0600

    Add test endpoint `get_users`

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::Router;
-use sea_orm::Database;
+use chrono::Utc;
+use entity::user;
+use sea_orm::ActiveValue::Set;
+use sea_orm::{ActiveModelTrait, Database};
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() {
@@ -11,8 +15,9 @@ async fn main() {
 }
 
 async fn server() {
-    let db = Database::connect("postgres://postgres:bloodyroots@localhost/axum-fullstack").await.unwrap();
-    let router = Router::new().route("/api/test", get(test));
+    let router = Router::new()
+        .route("/api/test", get(test))
+        .route("/api/user/create", get(get_users));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, router).await.unwrap();
@@ -21,4 +26,22 @@ async fn server() {
 async fn test() -> impl IntoResponse {
     println!("Test Api");
     (StatusCode::ACCEPTED, "Hello There")
+}
+
+async fn get_users() -> impl IntoResponse {
+    let db = Database::connect("postgres://postgres:bloodyroots@localhost/axum-fullstack")
+        .await
+        .unwrap();
+
+    let user = user::ActiveModel {
+        id: Default::default(),
+        name: Set("Shaphil".to_string()),
+        email: Set("mahmud@shaphil.me".to_string()),
+        password: Set("123456".to_string()),
+        uuid: Set(Uuid::new_v4()),
+        created_at: Set(Utc::now().naive_utc()),
+    };
+
+    let data = user.insert(&db).await.unwrap();
+    (StatusCode::ACCEPTED, data.name)
 }
