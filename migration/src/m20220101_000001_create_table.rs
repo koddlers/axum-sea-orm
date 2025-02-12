cedc97d51401b0d1210f1f808cdba2f809f3fc08
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2025-02-12 21:45:18 +0600

    Add Sea-ORM migration for `User`

---
+++ migration/src/m20220101_000001_create_table.rs
@@ -0,0 +1,41 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(User::Table)
+                    .if_not_exists()
+                    .col(pk_auto(User::Id))
+                    .col(ColumnDef::new(User::Name).string().not_null())
+                    .col(ColumnDef::new(User::Email).string().unique_key().not_null())
+                    .col(ColumnDef::new(User::Password).string().not_null())
+                    .col(ColumnDef::new(User::Uuid).uuid().unique_key().not_null())
+                    .col(ColumnDef::new(User::CreatedAt).date_time().not_null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(User::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum User {
+    Table,
+    Id,
+    Name,
+    Email,
+    Password,
+    Uuid,
+    CreatedAt,
+}
