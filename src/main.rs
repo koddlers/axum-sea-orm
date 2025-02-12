ccad7479997ecda7c317d31dc24c7976a5099077
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2025-02-12 21:46:02 +0600

    Add `sea-orm` to dependencies

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::Router;
+use sea_orm::Database;
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
     server().await;
 }
 
 async fn server() {
+    let db = Database::connect("postgres://postgres:bloodyroots@localhost/axum-fullstack").await.unwrap();
     let router = Router::new().route("/api/test", get(test));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
