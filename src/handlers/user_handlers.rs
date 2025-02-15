d1bcd865a750f815b0c3cdba9e1deabd3dadbcb5
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2025-02-15 22:36:03 +0600

    Add error handling to `handlers::user_handler::update_user`

--- src/handlers/user_handlers.rs
+++ src/handlers/user_handlers.rs
@@ -1,19 +1,18 @@
 use crate::models::user::{User, UserUpdateModel};
+use crate::utils::errors::APIError;
 use axum::extract::Path;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::{Extension, Json};
 use sea_orm::ActiveValue::Set;
-use sea_orm::{
-    ActiveModelTrait, ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter,
-};
+use sea_orm::{ActiveModelTrait, ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
 use uuid::Uuid;
 
 pub async fn update_user(
     Extension(db): Extension<DatabaseConnection>,
     Path(uuid): Path<Uuid>,
     Json(data): Json<UserUpdateModel>,
-) -> impl IntoResponse {
+) -> Result<Json<User>, APIError> {
     let mut user: entity::user::ActiveModel = entity::user::Entity::find()
         .filter(entity::user::Column::Uuid.eq(uuid))
         .one(&db)
@@ -23,8 +22,11 @@ pub async fn update_user(
         .into();
 
     user.name = Set(data.name);
-    user.clone().update(&db).await.unwrap();
-    // db.close().await.unwrap();
+    user.clone().update(&db).await.map_err(|err| APIError {
+        message: err.to_string(),
+        status_code: StatusCode::INTERNAL_SERVER_ERROR,
+        error_code: Some(50),
+    })?;
 
     // since we cannot convert `ActiveModel` to `Model`, we do that manually
     let user = user.clone();
@@ -36,7 +38,7 @@ pub async fn update_user(
         created_at: user.created_at.unwrap(),
     };
 
-    (StatusCode::ACCEPTED, Json(response))
+    Ok(Json(response))
 }
 
 pub async fn delete_user(
