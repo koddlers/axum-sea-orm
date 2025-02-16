63dac1b3ff8c1a4f4c8e219842b395482c99a596
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2025-02-17 02:38:57 +0600

    Add constant `TOKEN`

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -4,9 +4,15 @@ use std::env;
 
 lazy_static! {
     pub static ref DATABASE_URL: String = set_db();
+    pub static ref TOKEN: String = set_token();
 }
 
 fn set_db() -> String {
     dotenv().ok();
     env::var("DATABASE_URL").unwrap()
+}
+
+fn set_token() -> String {
+    dotenv().ok();
+    env::var("TOKEN").unwrap()
 }
